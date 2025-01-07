90703c4b4a7a9f67ebd141706a40e3d304f1e51d
kingdamie <damyridwan@gmail.com>
2025-01-07 23:11:55 +0100

    product gallery and view Product

--- src/pages/user-dashboard/ViewProduct.jsx
+++ src/pages/user-dashboard/ViewProduct.jsx
@@ -1,7 +1,9 @@
 // import { Link } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
+import Button from "@mui/material/Button";
 import routes from "../../routes";
 import BreadcrumbCard from "../../components/user-dashboard/BreadcrumbCard";
+import ProductGallery from "../../components/user-dashboard/ProductGallery";
 const ViewProduct = () => {
    const breadcrumbLinks = [
 			{ to: routes.userDashboard, icon: <FaHome />, label: "Dashboard" },
@@ -11,6 +13,27 @@ const ViewProduct = () => {
 	return (
 		<div className="right-content w-100">
 			<BreadcrumbCard title="View Product" breadcrumbLinks={breadcrumbLinks} />
+			<div className="d-flex gap-3 main-container view-product">
+				<ProductGallery />
+				<div className="full-product-review">
+					<h1>Complete School Management System</h1>
+					<p>
+						<span>product code:</span> #003222
+					</p>
+					<p>
+						<span>Brand:</span> EDUOS
+					</p>
+					<p>
+						<span>Secure with:</span> 90%
+					</p>
+					<div className="note">
+						Buy our Multi-functional, self-installable and Effortlessly easy
+						Educational portal, specially crafted for schools that are commited
+						to progress
+					</div>
+					<Button variant="contained">Continue Order</Button>
+				</div>
+			</div>
 		</div>
 	);
 };
